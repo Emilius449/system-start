d894c2856f51def7eb67a7d6c2e9dcd0f2085c7f
emilius mfuruki <emilius.mfuruki@ega.go.tz>
2023-07-14 16:30:08 +0300

    patch value

--- src/app/modules/forms/pages/form-array/form-array.component.ts
+++ src/app/modules/forms/pages/form-array/form-array.component.ts
@@ -5,6 +5,7 @@ import {
   FormControl,
   FormArray,
   ReactiveFormsModule,
+  FormBuilder,
 } from '@angular/forms';
 import { UtilityService } from 'src/app/shared/service/utility.service';
 
@@ -16,34 +17,48 @@ import { UtilityService } from 'src/app/shared/service/utility.service';
   styleUrls: ['./form-array.component.scss'],
 })
 export class FormArrayComponent {
-  addressFormGroup = new FormGroup({
-    street: new FormControl(''),
-    city: new FormControl(''),
-  });
+  form: FormGroup;
+  constructor(private fb: FormBuilder, private utilityService: UtilityService) {
+    this.form = this.fb.group({
+      firstName: [''],
+      lastName: [''],
+      address: this.fb.array([]),
+    });
+  }
 
-  form = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-    address: new FormArray([this.addressFormGroup]),
-  });
-
-  constructor(private utilityService: UtilityService) {}
-
-  get address() {
+  get address(): FormArray {
     return this.form.get('address') as FormArray;
   }
 
   addAddress() {
-    this.address.push(this.addressFormGroup);
+    const addressFormGroup = this.fb.group({
+      street: [''],
+      city: [''],
+    });
+    this.address.push(addressFormGroup);
   }
-  removeAddree(index: number) {
+
+  removeAddress(index: number) {
     this.address.removeAt(index);
   }
+
   onSubmit() {
-    console.log('Form Value: ', this.form.value);
+    console.log('Form Value: ', JSON.stringify(this.form.value));
     if (this.form.valid) {
+      // Perform necessary actions with the form data
     } else {
       this.utilityService.validateAllFormFields(this.form);
     }
+  }
+
+  patch() {
+    this.form.patchValue({
+      firstName: 'Brynne',
+      lastName: 'Allison',
+      address: [
+        { street: 'Aliqua Est consequa', city: 'Duis ipsum maxime v' },
+        { street: 'EMil', city: 'Dar' },
+      ],
+    });
   }
 }
